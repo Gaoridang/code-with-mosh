eedbcb795c674e2f80c8861d8fe2c144e716f825
gaori <monooi@naver.com>
2023-06-19 15:08:09 +0900

    test movie api

--- movies/src/App.tsx
+++ movies/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const options = {
+  method: "GET",
+  url: "https://streaming-availability.p.rapidapi.com/v2/services",
+  headers: {
+    "X-RapidAPI-Key": "b1bfbd60camshbc4e34ecf508454p1558e2jsn98def5fb1712",
+    "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
+  },
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+try {
+  const response = await axios.request(options);
+  console.log(response.data);
+} catch (error) {
+  console.error(error);
 }
 
-export default App
+function App() {
+  return <></>;
+}
+
+export default App;
