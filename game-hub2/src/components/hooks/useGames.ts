82321ae0efc267c689ebf21b645bd728845b24cf
gaori <monooi@naver.com>
2023-06-28 18:48:49 +0900

    making game list hook

---
+++ game-hub2/src/components/hooks/useGames.ts
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import apiClient from "../../services/api-client";
+
+interface Platform {
+  platform: {
+    id: number;
+    slug: string;
+    name: string;
+  };
+}
+
+interface Game {
+  name: string;
+  id: number;
+  slug: string;
+  background_image: string;
+  platforms: Platform[];
+  rating: number;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    apiClient
+      .get<FetchGamesResponse>("/games")
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setLoading(false);
+      });
+  }, []);
+  return { games, error, isLoading };
+};
+
+export default useGames;
