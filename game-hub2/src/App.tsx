7c6425c21c9ecb1c6bd55acef12522fc544be02e
gaori <monooi@naver.com>
2023-06-28 18:25:17 +0900

    responsive grid

--- game-hub2/src/App.tsx
+++ game-hub2/src/App.tsx
@@ -1,7 +1,36 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `'header' 'main' 'footer'`,
+          lg: `'header header' 'aside main' 'footer footer'`,
+        }}
+        gridTemplateRows={}
+        gridTemplateColumns={}
+        p={5}
+      >
+        <GridItem area={"header"}>
+          <Header />
+        </GridItem>
+        <Show above="lg">
+          <GridItem area={"aside"} bg={"red.400"}>
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area={"main"} bg={"yellow"}>
+          Main
+        </GridItem>
+        <GridItem area={"footer"} bg={"green"}>
+          Footer
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
